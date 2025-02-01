0d28e9682e54911b9eff8c33fb3126bdf0220ccd
tora-nosuke18 <tiger.tora1210@gmail.com>
2025-02-01 17:55:40 +0900

    change odom_frame_id & add delete previous data option

--- src/visual_odom/launch/gazebo_vslam.launch.py
+++ src/visual_odom/launch/gazebo_vslam.launch.py
@@ -1,14 +1,7 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
-
 
 def generate_launch_description():
-
     stereo_odometry_params = {
         'frame_id': 'base_link',
         'odom_frame_id': 'odom',
@@ -22,26 +15,17 @@ def generate_launch_description():
         'OdomF2M/MaxSize': '1000',
         'GFTT/MinDistance': '10',
         'Odom/MinInliers' : "50"
-
     }
 
-    pkg_stereo_image_proc = get_package_share_directory(
-        'stereo_image_proc')
-
-    # Paths
-    stereo_image_proc_launch = PathJoinSubstitution(
-        [pkg_stereo_image_proc, 'launch', 'stereo_image_proc.launch.py'])
-
     rtabmap_params = {
-        'frame_id': 'base_link',
-        'subscribe_stereo': True,
-        'subscribe_depth': False,
+        # 'odom_frame_id': "visual_odom",
+        'subscribe_stereo': False,
+        'subscribe_depth': True,
         'approx_sync': False,
         'wait_for_transform_duration': 0.2,
         'map_always_update': True,
         'queue_size': 10,
         'use_sim_time' : True,
-
         'GridGlobal/MinSize': '100',
         # 'Vis/MinInliers': 12,
         # 'Mem/UseOdomGravity': True,
@@ -51,33 +35,25 @@ def generate_launch_description():
     }
 
     remappings = [
-        ('odom', '/visual_odom'),
-        ('left/image_rect', '/infra1/image_raw'),
-        ('left/camera_info', '/infra1/camera_info'),
-        ('right/image_rect', '/infra2/image_raw'),
-        ('right/camera_info', '/infra2/camera_info')
+        ('odom', '/visual/odom'),
+        ('rgb/image', 'color/image_raw'),
+        ('rgb/camera_info', 'color/camera_info'),
+        ('depth/image', 'depth/depth/image_raw'),
     ]
 
-
     return LaunchDescription([      
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([stereo_image_proc_launch]),
-            launch_arguments=[
-                # ('left_namespace', 'stereo_camera/left'),
-                # ('right_namespace', 'stereo_camera/right'),
-                ('disparity_range', '256'),
-            ]
-        ),
-    
         Node(
-            package='rtabmap_odom', executable='stereo_odometry', 
-            parameters=[stereo_odometry_params],  # List format
-            remappings=remappings
+            package='rtabmap_odom', executable='rgbd_odometry', 
+            parameters=[stereo_odometry_params,
+                        {'odom_frame_id': "visual_odom"}],  # List format
+            remappings=remappings,
         ),
 
         Node(
             package='rtabmap_slam', executable='rtabmap', output='screen',
-            parameters=[rtabmap_params],  # List format
-            remappings=remappings,
+            parameters=[rtabmap_params,
+                        {'odom_frame_id': "visual_odom"}],  # List format
+            remappings=remappings,  # This will delete the previous database
+            arguments=['--delete_db_on_start'],
         ),
     ])
