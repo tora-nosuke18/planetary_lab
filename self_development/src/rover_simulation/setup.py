1dc305ed639940c18f8ea34dc9ecb62f5c7a0a0d
tora-nosuke18 <tiger.tora1210@gmail.com>
2025-03-13 16:23:21 +0900

    updated to launch world from rover_simulation/models files

--- self_development/src/rover_simulation/setup.py
+++ self_development/src/rover_simulation/setup.py
@@ -3,22 +3,32 @@ from setuptools import find_packages, setup
 from glob import glob
 package_name = 'rover_simulation'
 
+data_files = []
+data_files.append(("share/ament_index/resource_index/packages", ["resource/" + package_name]))
+data_files.append(("share/" + package_name, ["package.xml"]))
+
+def package_files(directory, data_files):
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            data_files.append(("share/" + package_name + "/" + path, glob(path + "/**/*.*", recursive=True)))
+    return data_files
+
+data_files = package_files('launch/', data_files)
+data_files = package_files('urdf/', data_files)
+data_files = package_files('meshes/', data_files)
+data_files = package_files('config/', data_files)
+data_files = package_files('world/', data_files)
+data_files = package_files('models/mars_terrain/', data_files)
+data_files = package_files('models/mars_terrain/meshes/', data_files)
+data_files = package_files('models/mars_terrain/meshes/scripts/', data_files)
+data_files = package_files('models/mars_terrain/meshes/textures/', data_files)
+
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
-        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
-        (os.path.join('share', package_name, 'config'), glob('config/*')),
-        (os.path.join('share', package_name, 'world'), glob('world/*')),
-        (os.path.join('share', package_name, 'models'), glob('models/term3_rover/*'))
-
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tora',
